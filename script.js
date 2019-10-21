3147e292fee8a8cb4d17b44e41830dae61271181
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-10-21 19:31:24 -0400

    added a statement for show players

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const box7 = document.querySelector("#box7")
 const box8 = document.querySelector("#box8")
 const box9 = document.querySelector("#box9")
 const reset = document.querySelector(".reset")
+let ptag = document.querySelector(".ptag")
 
 let click = 0
 for (let i = 0; i < box.length; i++) {
@@ -21,12 +22,13 @@ for (let i = 0; i < box.length; i++) {
         }
         if (click % 2 === 1) {
             evt.target.style.backgroundColor = "red"
-
             click += 1
+            ptag.innerText = ("It's player1's turn")
         }
         else if (click % 2 === 0) {
             evt.target.style.backgroundColor = "blue"
             click += 1
+            ptag.innerText = ("It's player2's turn")
         }
     setTimeout(gameWins, 1000)
 
@@ -53,6 +55,7 @@ function gameWins(){
         (box2.style.backgroundColor==="blue" && box5.style.backgroundColor==="blue" && box8.style.backgroundColor==="blue")||
         (box3.style.backgroundColor==="blue" && box6.style.backgroundColor==="blue" && box9.style.backgroundColor==="blue")){
             alert("Blue has won the Game")
+            ptag.innerText = ("")
             for (let j = 0; j < box.length; j++){
                 box[j].style.backgroundColor = "white"
             }
@@ -66,18 +69,16 @@ function gameWins(){
         (box2.style.backgroundColor === "red" && box5.style.backgroundColor === "red" && box8.style.backgroundColor === "red") ||
         (box3.style.backgroundColor === "red" && box6.style.backgroundColor === "red" && box9.style.backgroundColor === "red")) {
         alert("Red has won the Game")
+        ptag.innerText = ("")
         for (let j = 0; j < box.length; j++) {
             box[j].style.backgroundColor = "white"
         }
     }
     else if (click === 9){
         alert("There has been a tie")
+        ptag.innerText = ("")
         for (let j = 0; j < box.length; j++) {
         box[j].style.backgroundColor = "white"
         }
     }
 }
-
-// var newPara = document.createElement("p");
-// newPara.innerText = "Blue's turn";
-// document.querySelector('.pdiv')
