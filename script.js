0f399a875ba347b1f61bf1c1daa058dd8a4da207
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-10-21 16:19:15 -0400

    added round completion

--- script.js
+++ script.js
@@ -12,35 +12,74 @@ const box9 = document.querySelector("#box9")
 const reset = document.querySelector(".reset")
 
 let click = 0
- 
-
-// const box = document.querySelectorAll(".box")
 for (let i = 0; i < box.length; i++)
     box[i].addEventListener("click", function (evt) {
         evt.preventDefault()
-        // console.log(evt)
+        console.log(evt)
         if (evt.target.style.backgroundColor === "blue" || evt.target.style.backgroundColor === "red") {
             return
         }
-            if (click % 2 === 1) {
+        if (click % 2 === 1) {
             evt.target.style.backgroundColor = "red"
             click += 1
-            }
-            else if (click % 2 === 0) {
+        }
+        else if (click % 2 === 0) {
             evt.target.style.backgroundColor = "blue"
             click += 1
-            }
+        }
+    setTimeout(gameWins,1000)
 })
 
 reset.addEventListener("click", function(evt){
     evt.preventDefault()
-    console.log(evt)
+    // console.log(evt)
     click = 0
     for (let j = 0; j < box.length; j++){
         box[j].style.backgroundColor = "white"
     }
 })
 
-// const player2 = "blue";
-// const player1 = box.style.backgroundcolor
-// box.style.backgroundcolor = blue;
+function gameWins(){
+    if ((box1.style.backgroundColor==="blue" && box2.style.backgroundColor==="blue" && box3.style.backgroundColor==="blue")|| 
+        (box4.style.backgroundColor==="blue" && box5.style.backgroundColor==="blue" && box6.style.backgroundColor==="blue")||
+        (box7.style.backgroundColor==="blue" && box8.style.backgroundColor==="blue" && box9.style.backgroundColor==="blue")||
+        (box1.style.backgroundColor==="blue" && box5.style.backgroundColor==="blue" && box9.style.backgroundColor==="blue")||
+        (box3.style.backgroundColor==="blue" && box5.style.backgroundColor==="blue" && box7.style.backgroundColor==="blue")||
+        (box1.style.backgroundColor==="blue" && box4.style.backgroundColor==="blue" && box7.style.backgroundColor==="blue")||
+        (box2.style.backgroundColor==="blue" && box5.style.backgroundColor==="blue" && box8.style.backgroundColor==="blue")||
+        (box3.style.backgroundColor==="blue" && box6.style.backgroundColor==="blue" && box9.style.backgroundColor==="blue")){
+            alert("Blue has won the Game")
+            for (let j = 0; j < box.length; j++){
+                box[j].style.backgroundColor = "white"
+            }
+    }
+    else if ((box1.style.backgroundColor==="red" && box2.style.backgroundColor==="red" && box3.style.backgroundColor==="red")|| 
+        (box4.style.backgroundColor==="red" && box5.style.backgroundColor==="red" && box6.style.backgroundColor==="red")||
+        (box7.style.backgroundColor==="red" && box8.style.backgroundColor==="red" && box9.style.backgroundColor==="red")||
+        (box1.style.backgroundColor==="red" && box5.style.backgroundColor==="red" && box9.style.backgroundColor==="red")||
+        (box3.style.backgroundColor==="red" && box5.style.backgroundColor==="red" && box7.style.backgroundColor==="red")||
+        (box1.style.backgroundColor==="red" && box4.style.backgroundColor==="red" && box7.style.backgroundColor==="red")||
+        (box2.style.backgroundColor==="red" && box5.style.backgroundColor==="red" && box8.style.backgroundColor==="red")||
+        (box3.style.backgroundColor==="red" && box6.style.backgroundColor==="red" && box9.style.backgroundColor==="red")){
+            alert("Red has won the Game")
+            for (let j = 0; j < box.length; j++){
+                box[j].style.backgroundColor = "white"
+    }       }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
